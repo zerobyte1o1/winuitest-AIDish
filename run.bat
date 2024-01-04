648023998c27bb23b59e37cfed81ab5d7282eddd
刘方靖 <liufangjing@shifang.co>
2024-01-04 14:44:25 +0800

    page

--- run.bat
+++ run.bat
@@ -18,9 +18,12 @@ if %errorlevel% equ 0 (
     echo Port 8765 is in use. Stopping the process using that port...
     FOR /F "tokens=5" %%P IN ('netstat -ano ^| findstr :8765') DO TaskKill.exe /F /PID %%P
 )
-set "command=allure generate ./result -o ./result/report/ && allure serve ./result/ -p 8765"
+set "command=allure generate ./result -o ./result/report/"
 
 echo Running command: !command!
 !command!
 
+set "command=allure serve ./result/ -p 8765"
 
+echo Running command: !command!
+!command!
