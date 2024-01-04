8425ae30c8b072f562ec57db82c25c4c9e9e436b
刘方靖 <liufangjing@shifang.co>
2024-01-04 14:04:04 +0800

    page

--- run.bat
+++ run.bat
@@ -9,13 +9,9 @@ set "command=pytest testCase/ --alluredir=./result --clean-alluredir"
 echo Running command: !command!
 !command!
 
-set "command=allure generate ./result -c -o ./result/report/"
+set "command=allure generate ./result -c -o ./result/report/ && allure serve ./result/ -p 8765"
 
 echo Running command: !command!
 !command!
 
 
-set "command=allure serve ./result/ -p 8765"
-
-echo Running command: !command!
-!command!
