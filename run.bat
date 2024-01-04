c621ba3ee3dd965a8ee289cb33a8738b995c868d
刘方靖 <liufangjing@shifang.co>
2024-01-04 13:58:56 +0800

    page

--- run.bat
+++ run.bat
@@ -14,17 +14,6 @@ set "command=allure generate ./result -c -o ./result/report/"
 echo Running command: !command!
 !command!
 
-REM Check if port 8765 is in use and kill the process if necessary
-set "command=netstat -ano | findstr :8765"
-
-echo Running command: !command!
-!command!
-
-if %errorlevel% equ 0 (
-    set "command=FOR /F "tokens=5" %%P IN ('netstat -ano ^| findstr :8765') DO (taskkill /F /PID %%P)"
-    echo Running command: !command!
-    !command!
-)
 
 set "command=allure serve ./result/ -p 8765"
 
