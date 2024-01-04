acdd99f7df603a25a45860b817ce4b1ef8b2429e
刘方靖 <liufangjing@shifang.co>
2024-01-04 18:09:59 +0800

    page

--- pageObject/meals_pricing_page.py
+++ pageObject/meals_pricing_page.py
@@ -6,42 +6,81 @@ class MealsPricingPage(BasePage):
         super(MealsPricingPage, self).__init__()
 
     def order_mgr_loc(self):
+        """
+        获取订单管理元素
+        """
         return self.locate_element(title='订单')
 
     def food_management_loc(self):
+        """
+        获取菜品管理元素
+        """
         return self.locate_element(title='菜品')
 
     def open_img_loc(self):
+        """
+        获取打开图片元素
+        """
         return self.locate_element(title='打开')
 
     def open_img_file_name_loc(self):
+        """
+        获取打开图片文件名元素
+        """
         return self.locate_element(auto_id="1148")
 
     def open_img_ok_loc(self):
+        """
+        获取打开图片确定元素
+        """
         return self.locate_element(auto_id="1",title="打开(O)")
 
     def open_img_no_loc(self):
+        """
+        获取打开图片取消元素
+        """
         return self.locate_element(auto_id="2")
 
     def clear_machine_loc(self):
+        """
+        获取清机元素
+        """
         return self.locate_element(title='清机')
 
     def clear_machine_tips_loc(self):
+        """
+        获取清机提示元素
+        """
         return self.locate_element(title_re="^上次清机时间.*")
 
     def clear_machine_yes_loc(self):
+        """
+        获取清机确定元素
+        """
         return self.locate_element(auto_id='PageBase.widgetMain.CashierWidget.SFMessageForm.btnOk')
 
     def clear_machine_no_loc(self):
+        """
+        获取清机取消元素
+        """
         return self.locate_element(auto_id='PageBase.widgetMain.CashierWidget.SFMessageForm.btnCancel')
 
     def digital_selection_loc(self):
+        """
+        获取数字点选元素
+        """
         return self.locate_element(title="数字点选")
 
     def drink_loc(self):
+        """
+        获取酒水饮料元素
+        """
         return self.locate_element(title="酒水饮料")
 
     def non_standard_loc(self):
+        """
+        获取非标品元素
+        """
         return self.locate_element(title="非标品")
 
     def calculation_loc(self, digit: str):
@@ -53,34 +92,67 @@ class MealsPricingPage(BasePage):
         return self.locate_element(title=digit)
 
     def reidentify_loc(self):
+        """
+        获取重新识别元素
+        """
         return self.locate_element(title="重新\n识别")
 
     def merged_payment_loc(self):
+        """
+        获取合并支付元素
+        """
         return self.locate_element(title="合并\n支付")
 
     def auto_payment_loc(self):
+        """
+        获取自动支付元素
+        """
         return self.locate_element(title="自动\n支付")
 
     def cash_payment_loc(self):
+        """
+        获取现金支付元素
+        """
         return self.locate_element(title="现金\n支付")
 
     def cooperative_payment_loc(self):
+        """
+        获取合作支付元素
+        """
         return self.locate_element(title="合作\n支付")
 
     def hanging_orders_loc(self):
+        """
+        获取挂起订单元素
+        """
         return self.locate_element(title="挂起\n订单")
 
     def cancel_payment_loc(self):
+        """
+        获取取消支付元素
+        """
         return self.locate_element(title="取消\n支付")
 
     def confirm_payment_loc(self):
+        """
+        获取确认支付元素
+        """
         return self.locate_element(title="确认支付")
 
     def identification_of_areas_loc(self):
+        """
+        获取区域识别元素
+        """
         return self.locate_element(class_name="QImageViewer")
 
     def total_amount_loc(self):
+        """
+        获取总金额元素
+        """
         return self.locate_element(class_name="QPushButton", found_index=1)
 
     def promotion_amount_loc(self):
+        """
+        获取促销金额元素
+        """
         return self.locate_element(class_name="QPushButton", found_index=2)
--- testCase/test_clear_machine.py
+++ testCase/test_clear_machine.py
@@ -5,8 +5,8 @@ import allure
 import pytest
 
 
-@allure.epic("清机功能测试")
-@allure.feature("场景：操作清机功能")
+@allure.epic("模块：菜品计价")
+@allure.feature("场景：操作清机")
 class TestClearMachine:
     def setup_class(self):
         self.mph = MealsPricingHandler()
@@ -14,7 +14,7 @@ class TestClearMachine:
     def teardown(self):
         self.mph.app.kill(self.mph.process_id)
 
-    @allure.story("用例-操作并确定清机")
+    @allure.story("操作清机")
     @allure.title("清机_确认")
     @allure.description("该用例检查用户确认清机操作")
     @allure.testcase("http://192.168.10.26:8081/#/track/case/edit/2705207b-92ca-03c4-60b2-23230d424de3?projectId=9f25fcce-03ea-40a6-a911-e736e7af30ed", name="点击，跳转到对应用例的链接地址")
@@ -30,7 +30,7 @@ class TestClearMachine:
         with allure.step('校验是否已清机成功'):
             assert current_time in self.mph.text_clear_machine_tips_loc()
 
-    @allure.story("用例-操作并取消清机")
+    @allure.story("操作清机")
     @allure.title("清机_取消")
     @allure.description("该用例检查用户取消清机操作")
     @allure.testcase("http://192.168.10.26:8081/#/track/case/edit/45f9032c-8167-1c5c-8705-69cbd4eae7d5?projectId=9f25fcce-03ea-40a6-a911-e736e7af30ed", name="点击，跳转到对应用例的链接地址")
