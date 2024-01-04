776e437bd1bc78a1c062cc0c090dad2da107e3b9
刘方靖 <liufangjing@shifang.co>
2024-01-04 15:20:12 +0800

    page

--- run.bat
+++ run.bat
@@ -3,8 +3,11 @@
 setlocal enabledelayedexpansion
 
 pip install -r requirements.txt
-
-
+netstat -ano | findstr :8765
+if %errorlevel% equ 0 (
+    echo Port 8765 is in use. Stopping the process using that port...
+    FOR /F "tokens=5" %%P IN ('netstat -ano ^| findstr :8765') DO TaskKill.exe /F /PID %%P
+)
 
 set "command=pytest testCase/ --alluredir=./result"
 
@@ -13,22 +16,12 @@ echo Running command: !command!
 
 ping 127.0.0.1 -n 5 >nul
 
-netstat -ano | findstr :8765
-if %errorlevel% equ 0 (
-    echo Port 8765 is in use. Stopping the process using that port...
-    FOR /F "tokens=5" %%P IN ('netstat -ano ^| findstr :8765') DO TaskKill.exe /F /PID %%P
-)
+
 set "command=allure generate ./result -o ./result/report/"
 
 echo Running command: !command!
 !command!
 
 set "command=allure serve ./result/ -p 8765"
-echo Running command: !command!
-!command!
-
-echo Executing the command: !command!
-!command!
-
 echo Running command: !command!
 !command!
