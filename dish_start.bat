a37a011bcb858081b24cafecc0e3a28b2f1555e5
刘方靖 <liufangjing@shifang.co>
2024-01-13 15:20:54 +0800

    page

---
+++ dish_start.bat
@@ -0,0 +1,3 @@
+@REM rem 启动 yx-dish.exe 程序
+start  C:\"Program Files"\AIDish\Client\yx-dish.exe
+
--- run.bat
+++ run.bat
@@ -3,25 +3,7 @@
 setlocal enabledelayedexpansion
 
 pip install -r requirements.txt
-netstat -ano | findstr :8765
-if %errorlevel% equ 0 (
-    echo Port 8765 is in use. Stopping the process using that port...
-    FOR /F "tokens=5" %%P IN ('netstat -ano ^| findstr :8765') DO TaskKill.exe /F /PID %%P
-)
 
-set "command=pytest testCase/ --alluredir=./result"
-
-echo Running command: !command!
-!command!
-@REM
-@REM
-@REM
-@REM
-@REM set "command=allure generate ./result -o ./result/report/"
-@REM
-@REM echo Running command: !command!
-@REM !command!
-
-set "command=allure serve ./result/ -p 8765 &"
+set "command=allure serve ./result/ -p 8765 --no-open"
 echo Running command: !command!
 !command!
