4c5b3344670fed41c92ea8377a9bc5cb73886e42
刘方靖 <liufangjing@shifang.co>
2024-01-16 15:20:50 +0800

    page

--- testCase/conftest.py
+++ testCase/conftest.py
@@ -2,7 +2,7 @@ from utils.wechat_info import send_wechat_message
 import pytest
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session',autouse=True)
 def send_info(request):
     """
     发送企业微信消息
