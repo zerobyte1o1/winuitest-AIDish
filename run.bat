ddb3e92ca6b16edad7de221bab450fff62bf56f2
刘方靖 <liufangjing@shifang.co>
2024-01-24 15:40:29 +0800

    page

--- run.bat
+++ run.bat
@@ -3,8 +3,6 @@
 setlocal enabledelayedexpansion
 
 pip install -r requirements.txt
-pytest testCase/ --alluredir=./result/temp
-allure generate  ./result/temp -o ./result/allure-report
-allure-combine --auto-create-folders ./result/allure-report --dest c:/allure-html/1212/
+python run.py
 @REM allure serve ./result/ -p 8765
 
---
+++ run.py
@@ -0,0 +1,15 @@
+import subprocess
+from utils import env
+from utils.logger import log
+
+try:
+    with open(env.get_listen_file(), "r") as file:
+        version = file.readline().strip()
+except FileNotFoundError:
+    log.warning("env.get_listen_file() no listen file")
+
+subprocess.call(["pytest", "testCase/", "--alluredir=./result/temp"])
+subprocess.call(["allure", "generate", "./result/temp", "-o", "./result/allure-report"])
+subprocess.call(["allure-combine", "--auto-create-folders", "./result/allure-report", "--dest", "c:/allure-html/"+version])
+
+
