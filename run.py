640560217e9d0f3c540cfdd3a4ff3e9b4900232a
刘方靖 <liufangjing@shifang.co>
2024-01-24 16:28:05 +0800

    page

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ try:
         version = file.readline().strip()
 except FileNotFoundError:
     log.warning("env.get_listen_file() no listen file")
-path=os.path.join(os.path.abspath(),"result/allure-report")
+path=os.path.join(os.path.abspath('./'),"result/allure-report")
+print(path)
 subprocess.call(["allure-combine", "--auto-create-folders",path, "--dest", "c:/allure-html/"+version])
 
 
