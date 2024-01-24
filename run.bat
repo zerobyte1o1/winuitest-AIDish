53755b2159ade8d0cf59e61c60d561cc3e8a0a9d
刘方靖 <liufangjing@shifang.co>
2024-01-24 17:09:14 +0800

    page

--- run.bat
+++ run.bat
@@ -5,6 +5,7 @@ setlocal enabledelayedexpansion
 pip install -r requirements.txt
 pytest testCase/ --alluredir=./result/temp
 allure generate ./result/temp -o ./result/allure-report
+timeout /t 5 >nul
 set /p version=<c:/dish.listen
 allure-combine --auto-create-folders ./result/allure-report --dest c:/allure-html/%version%
 
