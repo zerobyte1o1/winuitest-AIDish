6acb8483076c934b192e5a39bb02b124335fb909
刘方靖 <liufangjing@shifang.co>
2024-01-25 16:33:15 +0800

    page

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ set /p version=<c:/dish.listen
 
 start /B allure generate --clean ./result/temp -o ./result/allure-report
 
-timeout /t 3 >nul
+timeout /t 5 >nul
 
 start /SEPARATE allure-combine --auto-create-folders ./result/allure-report --dest c:/allure-html/%version%
 
