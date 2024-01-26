a7529ebc11410cb7b022cd0cbc008892930fbec5
刘方靖 <liufangjing@shifang.co>
2024-01-26 16:14:13 +0800

    page

--- report.bat
+++ report.bat
@@ -1,5 +1,3 @@
 set /p version=<c:/dish.listen
-allure-combine --auto-create-folders c:/winuitest-AIDish/result/allure-report --dest c:/allure-html/%version%
 
-timeout /t 5 > nul
-allure-combine c:/winuitest-AIDish/result/allure-report --dest c:/allure-html/%version%
+start /B allure-combine --auto-create-folders c:/winuitest-AIDish/result/allure-report --dest c:/allure-html/%version%
--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 pip install -r requirements.txt
 pytest testCase/ --alluredir=./result/temp
 
-start /B allure generate --clean ./result/temp -o ./result/allure-report
+start /B /WAIT allure generate --clean ./result/temp -o ./result/allure-report
 
 call timeout /t 5 > nul&report.bat
 
