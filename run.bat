38ce538eb51fbcdb4eb6d20b0cb8f462224dce9f
刘方靖 <liufangjing@shifang.co>
2024-01-26 15:27:29 +0800

    page

--- run.bat
+++ run.bat
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 pytest testCase/ --alluredir=./result/temp
 
 start /B allure generate --clean ./result/temp -o ./result/allure-report
+timeout /t 5 > nul
 report.bat
 
 
