2fbe5069478f51493d38c6ff2d09d5e1ae93967a
刘方靖 <liufangjing@shifang.co>
2024-01-26 13:50:56 +0800

    page

--- run.bat
+++ run.bat
@@ -8,7 +8,9 @@ pytest testCase/ --alluredir=./result/temp
 start /B allure generate --clean ./result/temp -o ./result/allure-report
 
 
-set /p version=<c:/dish.listen
-@REM start /B allure-combine --auto-create-folders ./result/allure-report --dest c:/allure-html/%version%
 
-start /B allure-combine --auto-create-folders ./result/allure-report --dest ./result/res
+
+timeout /t 5 >nul
+set /p version=<c:/dish.listen
+allure-combine --auto-create-folders ./result/allure-report --dest c:/allure-html/%version%
+
