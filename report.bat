670800e91490a080423e6c0bf35c2d2b3b71c42f
刘方靖 <liufangjing@shifang.co>
2024-01-26 15:56:29 +0800

    page

--- report.bat
+++ report.bat
@@ -2,4 +2,4 @@ set /p version=<c:/dish.listen
 allure-combine --auto-create-folders c:/winuitest-AIDish/result/allure-report --dest c:/allure-html/%version%
 
 timeout /t 5 > nul
-allure-combine --auto-create-folders c:/winuitest-AIDish/result/allure-report --dest c:/allure-html/%version%
+start "" allure-combine c:/winuitest-AIDish/result/allure-report --dest c:/allure-html/%version%
