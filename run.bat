20d70eca064685f139a1e8b9a45d3c735ad867a5
刘方靖 <liufangjing@shifang.co>
2024-01-26 16:11:37 +0800

    page

--- run.bat
+++ run.bat
@@ -7,6 +7,6 @@ pytest testCase/ --alluredir=./result/temp
 
 start /B allure generate --clean ./result/temp -o ./result/allure-report
 
-call timeout /t 5 > nul&&report.bat
+call timeout /t 5 > nul&report.bat
 
 
